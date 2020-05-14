72d7394496eb4eb81100d26f9187f83c26d2da30
lilirdz <lilimrod@gmail.com>
2020-05-13 21:27:22 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,13 +5,18 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect to "/pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
@@ -19,8 +24,26 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end 
+
   patch '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
+    ####### bug fix
+    if !params[:pet].keys.include?("owner_id")
+    params[:pet]["owner_id"] = []
+    end 
+    #######
+  
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect to "/pets/#{@pet.id}"
   end
 end
